64c803eafe54d04a49673e97d07655cf5f95067b
SOO Ulrich <sessoklinaulrich@gmail.com>
2026-02-20 09:34:24 +0000

    remove all

--- src/modules/user/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * User Module Index
- * Re-exports all user module components
- */
-
-export { default as User } from "./user.model";
-export { default as userService } from "./user.service";
-export { default as userController } from "./user.controller";
-export { default as userRoutes } from "./user.routes";
