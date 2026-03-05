cfe26ec6501dff114624c7cf566f88975662f78e
SOO Ulrich <sessoklinaulrich@gmail.com>
2026-03-05 18:11:14 +0000

    refactor: update password hashing to use argon2 and clean up dependencies

--- src/utils/password.utils.ts
+++ src/utils/password.utils.ts
@@ -1,13 +1,14 @@
-import * as bcrypt from "bcrypt"
+import { argon2id, hash, verify } from "argon2";
 import env from "@config/env";
 
 export const hashWord = async (word: string): Promise<string> => {
-    return await bcrypt.hash(word, env.BCRYPT_SALT_ROUNDS);
+    return await hash(word, { type: argon2id, salt: Buffer.allocUnsafe(env.BCRYPT_SALT_ROUNDS) });
 }
 
 export const compareHash = async (hash: string, word: string): Promise<boolean> => {
-    // if (!hash || !word) return false;
-    return await bcrypt.compare(word, hash);
+    if (!hash || !word) return false;
+
+    return await verify(hash, word);
 }
 
 export default {
