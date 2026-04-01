4f2b383c9c9e84204a67beb1f8d202c1b31e24a0
KevInTech-Dev <devkevintech@gmail.com>
2026-04-01 12:56:55 +0000

    feat: Add authorization to refund payment routes

--- src/modules/Refund/refund.routes.ts
+++ src/modules/Refund/refund.routes.ts
@@ -2,18 +2,20 @@ import { Router } from "express";
 import { RefundController } from "./refund.controller";
 import { validate } from "../middleware/validate.middleware";
 import { allowRefundSchema, refundIdSchema, refundPaymentPagination, RefundSchema } from "./refund.schema";
+import authMiddleware from "../middleware/authMiddleware";
+import { authorize } from "../middleware/authorization.middleware";
 
 const router: Router = Router();
-
+router.use(authMiddleware);
 const refundPaymentController = new RefundController();
 
-router.post('', validate(RefundSchema, "body"), refundPaymentController.initializeRefundPayment);
+router.post('', authorize(['tenant']), validate(RefundSchema, "body"), refundPaymentController.initializeRefundPayment);
 
-router.get('', validate(refundPaymentPagination, 'query'), refundPaymentController.getRefundPaymentsPaginated);
+router.get('', authorize(['tenant', 'landlord', 'admin']), validate(refundPaymentPagination, 'query'), refundPaymentController.getRefundPaymentsPaginated);
 
-router.get('/:id', validate(refundIdSchema, 'params'), refundPaymentController.getRefundPaymentById);
+router.get('/:id', authorize(['tenant', 'landlord', 'admin']), validate(refundIdSchema, 'params'), refundPaymentController.getRefundPaymentById);
 
-router.patch('/:id', validate({
+router.patch('/:id', authorize(['landlord', 'admin']), validate({
     'query' : refundIdSchema,
     'body' : allowRefundSchema
 }), refundPaymentController.approveRefund);
